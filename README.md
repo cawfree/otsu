c6ebeb404774e44cd92809bb34e47fe5b510b274
cawfree <hello@cawfree.com>
2020-02-05 16:45:38 +0000

    Published to GitHub! 🚀

--- README.md
+++ README.md
@@ -1,14 +1,56 @@
 # otsu
-[Otsu's Method](https://en.wikipedia.org/wiki/Thresholding_(image_processing)) for automated thresholding in JavaScript.
+[Otsu's Method](https://en.wikipedia.org/wiki/Thresholding_(image_processing)) is way of implementing automatic thresholding. It is a binarization algorithm which can be used to select the optimal threshold between foreground and background quantities based on finding the minimum point of [cross variance](). This is particularly useful when performing segmentation in [image processing]().
 
-binarization
-monochrome
-Nobuyuki Otsu
+<a href="#badge">
+    <img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square">
+</a>
 
-https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Image_processing_post_otsus_algorithm.jpg/440px-Image_processing_post_otsus_algorithm.jpg
+<br />
+<br />
 
-http://www.labbookpages.co.uk/software/imgProc/files/otsuOrig.png
+<p align="center">
+  <img src="./public/image.jpg" width="512" height="384" />
+</p>
 
-great description http://www.labbookpages.co.uk/software/imgProc/otsuThreshold.html
+You can find an awesome description of the algorithm, which inspired this repository, [here](http://www.labbookpages.co.uk/software/imgProc/otsuThreshold.html).
 
-https://github.com/hipersayanX
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```bash
+npm install --save otsu
+```
+
+Using [`yarn`]():
+
+```bash
+yarn add otsu
+```
+## ✍️ Usage
+
+It's really simple to find the threshold of your image. All you have to do is supply the data!
+
+In the example below, we have a one-dimensional array of intensity data. Most greyscale image formats, such as [MNIST](http://yann.lecun.com/exdb/mnist/), can be fed directly into otsu this way. However, images that consist of multiple attributes per pixel (such as `(r,g,b)`) will first require pre-processing.
+
+```javascript
+import otsu from 'otsu';
+
+const intensity = [255, 0, 128, 4, 95 ...];
+
+const t = otsu(image);
+
+console.log(t); // i.e. 128
+
+const bw = image.map(e => (e > t ? 1 : 0));
+```
+
+## ✌️ License
+[MIT](https://opensource.org/licenses/MIT)
+
+
+<p align="center">
+  <a href="https://www.buymeacoffee.com/cawfree">
+    <img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy @cawfree a coffee" width="232" height="50" />
+  </a>
+</p>
--- src/index.js
+++ src/index.js
@@ -1,49 +1,44 @@
-import compose from 'rippleware';
+import compose from "rippleware";
 
-const histo = (data, bins) => data
-  .reduce(
-    (arr, e) => {
-      arr[bins.indexOf(e)] += 1;
-      return arr;
-    },
-    [...Array(bins.length)]
-      .fill(0),
-  );
+const histo = (data, bins) =>
+  data.reduce((arr, e) => {
+    arr[bins.indexOf(e)] += 1;
+    return arr;
+  }, [...Array(bins.length)].fill(0));
 
-const width = histogram => histogram.reduce((e, i) => (e + i), 0);
+const width = histogram => histogram.reduce((e, i) => e + i, 0);
 
-const bins = data => Array
-  .from(new Set(data))
-  .sort((e0, e1) => (e0 - e1));
+const bins = data => Array.from(new Set(data)).sort((e0, e1) => e0 - e1);
 
-const weight = (data, total) => data
-  .reduce((e, i) => (e + i), 0) / total;
+const weight = (data, total) => data.reduce((e, i) => e + i, 0) / total;
 
-const mean = (histogram, bins) => histogram.reduce((r, e, i) => r + (e * bins[i]), 0) / width(histogram);
+const mean = (histogram, bins) =>
+  histogram.reduce((r, e, i) => r + e * bins[i], 0) / width(histogram);
 
-const variance = (histogram, bins, mean) => histogram.reduce((r, e, i) => r + ((bins[i] - mean) * (bins[i] - mean)) * e, 0) / width(histogram);
+const variance = (histogram, bins, mean) =>
+  histogram.reduce(
+    (r, e, i) => r + (bins[i] - mean) * (bins[i] - mean) * e,
+    0
+  ) / width(histogram);
 
 const props = (hf, bf, total) => [
   weight(hf, total),
-  variance(hf, bf, mean(hf, bf)),
+  variance(hf, bf, mean(hf, bf))
 ];
 
 const cross = (wb, vb, wf, vf) => wb * vb + wf * vf;
 
-const otsu = (data) => {
+const otsu = data => {
   const b = bins(data);
   const h = histo(data, b);
   const { length: total } = data;
-  const vars = [...Array(b.length)]
-    .map(
-      (_, i) => {
-        const [wb, vb] = props(h.slice(0, i), b.slice(0, i), total);
-        const [wf, vf] = props(h.slice(i, h.length), b.slice(i, h.length), total);
-        const x = cross(wb, vb, wf, vf);
-        return !isNaN(x) ? x : Number.POSITIVE_INFINITY;
-      },
-    );
+  const vars = [...Array(b.length)].map((_, i) => {
+    const [wb, vb] = props(h.slice(0, i), b.slice(0, i), total);
+    const [wf, vf] = props(h.slice(i, h.length), b.slice(i, h.length), total);
+    const x = cross(wb, vb, wf, vf);
+    return !isNaN(x) ? x : Number.POSITIVE_INFINITY;
+  });
   return b[vars.indexOf(Math.min(...vars))];
 };
 
-export default compose().use('[Number]', data => otsu(data));
+export default compose().use("[Number]", data => otsu(data));
